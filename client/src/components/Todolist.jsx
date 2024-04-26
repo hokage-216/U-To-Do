aa294fdf97fcb35673e39dc345d38238f8065648
DomSimonetta <Guitardudedom@gmail.com>
2024-04-25 20:33:11 -0400

    basic todo jsx

---
+++ client/src/components/Todolist.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h1 className="card-title text-center">To-Do</h1>
+                <input type="todo" classname="todo-box" id="todo" />
+              </div>
+            </div>
+          </div>
+        </div>
+       </div>
+
+    )
+}
