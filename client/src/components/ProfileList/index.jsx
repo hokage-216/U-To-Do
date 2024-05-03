647d21af3f336102948365d7622e6d724fa4e3e4
mcook2323 <mattcook2323@gmail.com>
2024-05-03 14:35:32 -0400

    changed CSS for list

--- client/src/components/ProfileList/index.jsx
+++ client/src/components/ProfileList/index.jsx
@@ -7,28 +7,27 @@ const ProfileList = ({ profiles, title }) => {
 
   return (
     <div>
-      <h3 className="text-primary">{title}</h3>
-      <div className="flex-row justify-space-between my-4">
+      <h3 className="profile-list-title">{title}</h3>
+      <div className="profile-list-container">
         {profiles &&
           profiles.map((profile) => (
-            <div key={profile._id} className="col-12 col-xl-6">
-              <div className="card mb-3">
-                <h4 className="card-header bg-dark text-light p-2 m-0">
-                  {profile.name} <br />
-                  <span className="text-white" style={{ fontSize: '1rem' }}>
-                    currently has {profile.todos ? profile.todos.length : 0}{' '}
-                   Todo items
-                    {profile.todos && profile.todos.length === 1 ? '' : 's'}
-                  </span>
-                </h4>
-
-                <Link
-                  className="btn btn-block btn-squared btn-light text-dark"
-                  to={`/profiles/${profile._id}`}
-                >
-                  View their todos
-                </Link>
+            <div key={profile._id} className="profile-card">
+              <div className="profile-card-header">
+                {profile.name}
+                <br />
+                <span className="todo-count">
+                  currently has {profile.todos ? profile.todos.length : 0}{' '}
+                  Todo items
+                  {profile.todos && profile.todos.length === 1 ? '' : 's'}
+                </span>
               </div>
+
+              <Link
+                className="view-todos-link"
+                to={`/profiles/${profile._id}`}
+              >
+                View their todos
+              </Link>
             </div>
           ))}
       </div>
@@ -37,80 +36,3 @@ const ProfileList = ({ profiles, title }) => {
 };
 
 export default ProfileList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import { ADD_TODO, REMOVE_TODO } from '../../utils/mutations';
-
-// const TodoList = ({ todos }) => {
-//   const [newTodo, setNewTodo] = useState('');
-//   const [addTodo] = useMutation(ADD_TODO);
-//   const [removeTodo] = useMutation(REMOVE_TODO);
-
-//   const handleAddTodo = async (e) => {
-//     e.preventDefault(); // Prevent default form submission behavior
-//     try {
-//       // Validate newTodo
-//       if (!newTodo.trim()) {
-//         console.error('Todo cannot be empty');
-//         return;
-//       }
-//       const profileId = '6632d80fc0204635471d8817';
-//       // Call the addTodo mutation
-//       await addTodo({
-//         variables: { profileId: profileId, todos: newTodo },
-//       });
-//       // Reset the input field after adding the todo
-//       setNewTodo('');
-//     } catch (error) {
-//       console.error('Error adding todo:', error);
-//     }
-//   };
-
-//   const handleRemoveTodo = async (todoId) => {
-//     try {
-//       // Call the removeTodo mutation
-//       await removeTodo({
-//         variables: { todoId },
-//       });
-//     } catch (error) {
-//       console.error('Error removing todo:', error);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleAddTodo} className="todo-container">
-//       <input
-//         type="text"
-//         value={newTodo}
-//         onChange={(e) => setNewTodo(e.target.value)}
-//         placeholder="Enter a new todo"
-//         className="todo-input"
-//       />
-//       <button type="submit" className="todo-button">Add Todo</button>
-
-//       {/* Display existing todos if todos exist */}
-//       {todos && todos.map((todo) => (
-//         <div key={todo._id} className="todo-item">
-//           <p>{todo.todo}</p>
-//           {/* Button to remove todo */}
-//           <button onClick={() => handleRemoveTodo(todo._id)} className="todo-remove-button">Remove</button>
-//         </div>
-//       ))}
-//     </form>
-//   );
-// };
-
-// export default TodoList;
